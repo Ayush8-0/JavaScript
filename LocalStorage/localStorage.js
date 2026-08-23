a382d63533ab64ceca2ffbe039f9b8a3192d85c9
Ayush Kumthekar <ayushkumthekar07@gmail.com>
2026-08-23 14:19:40 +0530

    localStorage final Commit

--- LocalStorage/localStorage.js
+++ LocalStorage/localStorage.js
@@ -14,5 +14,43 @@ localStorage.setItem("data",newX);  //Update Data   //data 200
 localStorage.setItem("Animal", newStr); //Update Data   //Animal Lion
 console.log(localStorage.getItem('data'), localStorage.getItem('Animal'))   //200 Lion
 
+localStorage.removeItem('Animal');  //Removes element data like animal
+localStorage.clear()        //Clear the entirew localStorage
+
+let user = {
+    userName: "John",
+    place: "New York",
+    mobile: 1234567890 
+};
+
+localStorage.setItem("user", user); //user [object Object]
+localStorage.setItem("user1", JSON.stringify(user))   
+//user1  {"userName":"John","place":"New York","mobile":1234567890}	
+//let userAns = 
+console.log(JSON.parse(localStorage.getItem("user1")))
+// {userName: 'John', place: 'New York', mobile: 1234567890}
+// mobile
+// : 
+// 1234567890
+// place
+// : 
+// "New York"
+// userName
+// : 
+// "John"
+// [[Prototype]]
+// : 
+// Object
+
+//Boolean Values
+localStorage.setItem("isLoggedIn", true);
+localStorage.setItem("users", ['John', 'James', 'Jack']);
+
+console.log(localStorage.getItem("users"))
+
+
+
+
+
 
 
